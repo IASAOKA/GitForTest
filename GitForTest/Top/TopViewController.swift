93526efba075b2fa0b75847e63166b1694044c69
Isami Asaoka <i.asaoka7@gmail.com>
2021-07-26 09:19:37 +0900

    create top page

---
+++ GitForTest/Top/TopViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TopViewController.swift
+//  GitForTest
+//
+//  Created by 浅岡勇海 on 2021/07/26.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+}
+
+// MARK: - method
+extension TopViewController {
+}
+
